d7381f1f4a4af4454d8f773a44827c554c8a052e
Ahmed Emad <1600137@eng.asu.edu.eg>
2020-05-07 01:20:56 +0200

    AE

--- after_run_gui.py
+++ after_run_gui.py
@@ -52,8 +52,10 @@ class running :
             self.seg_b.grid(row=2, columnspan=2)
 
         else :
-            pass
-            # update and draw
+            # update and draw flag
+            flag = FALSE
+            if flag is TRUE : pass
+
 
     def str_seg(self):
         name_of_seg = self.segs_name_entry.get()
