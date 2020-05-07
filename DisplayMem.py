b477403be34c73e62601298257f18e3367cbce52
1600178@eng.asu.edu.eg <1600178@eng.asu.edu.eg>
2020-05-07 06:13:19 +0200

    hhhhhhhhhhhh

--- DisplayMem.py
+++ DisplayMem.py
@@ -6,7 +6,7 @@ class displayMem:
         x = 0
         for i in List_of_Sigments:
             s = i.getName() + "\n" + " ( "+str(i.getStartingAddress())+" : " + str(i.getStartingAddress() + i.getSize()) + " )"
-            e = Label(Master, text=s, borderwidth=1, relief="solid", width=20 , height= 2,bg="grey",font="arial 15 italic" )
+            e = Label(Master, text=s, borderwidth=1, relief="solid", width=20 , height= 2,bg="grey",font="arial 17 italic" )
             e.grid(row=int(x), column=0)
             if x == int(x + (i.getEndingAddress() - i.getStartingAddress())):
                 x = x + 1
