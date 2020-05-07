9c4b39689239b7403346d8d6471743296e66799a
1600178@eng.asu.edu.eg <1600178@eng.asu.edu.eg>
2020-05-07 23:08:21 +0200

    EDited

--- user_interface_gui.py
+++ user_interface_gui.py
@@ -7,13 +7,13 @@ class interface :
         self.f2=Frame(master)
         self.f=Frame(master)
         self.f.pack()
-        self.mem_size=Label(self.f,text=" memory size ")
+        self.mem_size=Label(self.f,text=" Memory size ",font="arial 12 italic")
         self.mem_size.grid(row=0,column=0)
         self.mem_entry=Entry(self.f)
         self.mem_entry.grid(row=0,column=1)
-        self.add_hole_b=Button(self.f,text=" add new hole ",command=self.add_hole)
+        self.add_hole_b=Button(self.f,text=" Add new hole ",command=self.add_hole,font="arial 12 italic",width=10 )
         self.add_hole_b.grid(row=1,columnspan=2)
-        self.ok = Button(self.f, text=" run ", command=self.run)
+        self.ok = Button(self.f, text=" Run ", command=self.run,font="arial 12 italic",width=5 )
         self.ok.grid(row=3, columnspan=2)
 
     def add_hole(self):
@@ -21,15 +21,15 @@ class interface :
         self.ok["state"] = "disabled"
         self.hole_frame =Frame(self.f)
         self.hole_frame.grid(row=2)
-        self.hole_start = Label(self.hole_frame, text=" hole starting address ")
+        self.hole_start = Label(self.hole_frame, text=" Hole starting address ",font="arial 12 italic")
         self.hole_start.grid(row=0, column=0)
         self.hole_start_entry = Entry(self.hole_frame)
         self.hole_start_entry.grid(row=0, column=1)
-        self.hole_sizee = Label(self.hole_frame, text=" hole size ")
+        self.hole_sizee = Label(self.hole_frame, text=" Hole size ",font="arial 12 italic")
         self.hole_sizee.grid(row=1, column=0)
         self.hole_size_entry = Entry(self.hole_frame)
         self.hole_size_entry.grid(row=1, column=1)
-        self.add_hole1_b = Button(self.hole_frame, text=" add  ", command=self.str_hole)
+        self.add_hole1_b = Button(self.hole_frame, text=" Add  ", command=self.str_hole,font="arial 12 italic")
         self.add_hole1_b.grid(row=2, columnspan=2)
 
     def str_hole(self):
