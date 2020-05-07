76855d7ec8c4367f293c1ff61b19c47d20fdb7c8
1600178@eng.asu.edu.eg <1600178@eng.asu.edu.eg>
2020-05-07 23:21:38 +0200

    edit tany

--- after_run_gui.py
+++ after_run_gui.py
@@ -23,8 +23,8 @@ class running :
         self.allocate_binding = Button(self.left_frame, text="  Allocate Binding Process ",font="arial 15 italic",width=20 , height= 2, command=self.binding)
         self.allocate_binding.grid(row=0, column=3)
         self.allocate_binding["state"] = "disabled"
-        mem=displayMem()
-        mem.drawMem(self.mem_frame,self.mm.getListOfAllPartitions())
+        self.mem=displayMem()
+        self.mem.drawMem(self.mem_frame,self.mm.getListOfAllPartitions())
 
     def add_process(self):
         self.add_process_b["state"] = "disabled"
@@ -33,15 +33,15 @@ class running :
         self.allocate_binding["state"] = "disabled"
         self.process_frame=Frame(self.left_frame)
         self.process_frame.grid(row=1)
-        self.no_of_segment = Label(self.process_frame, text=" no of segment ")
+        self.no_of_segment = Label(self.process_frame, text=" No of segment ",font="arial 12 italic")
         self.no_of_segment.grid(row=0, column=0)
-        self.no_of_segment_entry = Entry(self.process_frame)
+        self.no_of_segment_entry = Entry(self.process_frame,font="arial 12 italic")
         self.no_of_segment_entry.grid(row=0, column=1)
-        self.fb = Label(self.process_frame, text=" type(firstFit/bestFit) ")
+        self.fb = Label(self.process_frame, text=" Type(firstFit/bestFit) ",font="arial 12 italic")
         self.fb.grid(row=1, column=0)
-        self.fb_entry = Entry(self.process_frame)
+        self.fb_entry = Entry(self.process_frame,font="arial 12 italic")
         self.fb_entry.grid(row=1, column=1)
-        self.no_of_segment_b = Button(self.process_frame, text=" ok ", command=self.fill_process)
+        self.no_of_segment_b = Button(self.process_frame, text=" OK ", command=self.fill_process,font="arial 12 italic")
         self.no_of_segment_b.grid(row=2, columnspan=2)
 
     def fill_process(self):
@@ -57,15 +57,15 @@ class running :
             self.num_of_seg = self.num_of_seg -1
             self.segs_frame = Frame(self.left_frame)
             self.segs_frame.grid(row=1)
-            self.segs_name = Label(self.segs_frame, text=" name of segment ")
+            self.segs_name = Label(self.segs_frame, text=" Name of segment ",font="arial 12 italic")
             self.segs_name.grid(row=0, column=0)
-            self.segs_name_entry = Entry(self.segs_frame)
+            self.segs_name_entry = Entry(self.segs_frame,font="arial 12 italic")
             self.segs_name_entry.grid(row=0, column=1)
-            self.segs_sizee = Label(self.segs_frame, text=" segment size ")
+            self.segs_sizee = Label(self.segs_frame, text=" Segment size ",font="arial 12 italic")
             self.segs_sizee.grid(row=1, column=0)
-            self.segs_sizee_entry = Entry(self.segs_frame)
+            self.segs_sizee_entry = Entry(self.segs_frame,font="arial 12 italic")
             self.segs_sizee_entry.grid(row=1, column=1)
-            self.seg_b = Button(self.segs_frame, text=" ok  ", command=self.str_seg)
+            self.seg_b = Button(self.segs_frame, text=" OK  ", command=self.str_seg,font="arial 12 italic")
             self.seg_b.grid(row=2, columnspan=2)
         else :
             self.full_flag =not(self.mm.addProcess())
@@ -74,8 +74,11 @@ class running :
             self.table_b["state"] = "normal"
             if self.full_flag == True:  self.allocate_binding["state"] = "normal"
             if self.full_flag == False :
-                # drawmem
-                pass
+                self.mem_frame.pack_forget()
+                self.mem_frame.destroy()
+                self.mem_frame = Frame(self.master)
+                self.mem_frame.pack(side=RIGHT)
+                self.mem.drawMem(self.mem_frame, self.mm.getListOfAllPartitions())
             else :
                 self.biind()
 
@@ -99,11 +102,11 @@ class running :
         self.allocate_binding["state"] = "disabled"
         self.table_frame = Frame(self.left_frame)
         self.table_frame.grid(row=1)
-        process_number = Label(self.table_frame, text=" Enter process name ")
+        process_number = Label(self.table_frame, text=" Enter process name ",font="arial 12 italic")
         process_number.grid(row=1,column=0)
-        ok_button=Button(self.table_frame,text="OK",command=self.delete_frame_show_table)
+        ok_button=Button(self.table_frame,text="OK",command=self.delete_frame_show_table,font="arial 12 italic")
         ok_button.grid(row=2,column=3)
-        self.enter_process_forTable=Entry(self.table_frame)
+        self.enter_process_forTable=Entry(self.table_frame,font="arial 12 italic")
         self.enter_process_forTable.grid(row=1,column=1)
 
     def delete_frame_show_table(self):
@@ -125,11 +128,11 @@ class running :
         self.allocate_binding["state"] = "disabled"
         self.del_process_frame = Frame(self.left_frame)
         self.del_process_frame.grid(row=1)
-        process_no = Label(self.del_process_frame, text=" Enter process name ")
+        process_no = Label(self.del_process_frame, text=" Enter process name ",font="arial 12 italic")
         process_no.grid(row=1, column=0)
-        ok_button = Button(self.del_process_frame, text="OK", command=self.delete_frame_update_memDiagram)
+        ok_button = Button(self.del_process_frame, text="OK", command=self.delete_frame_update_memDiagram,font="arial 12 italic")
         ok_button.grid(row=2, column=3)
-        self.enter_process_for_delete = Entry(self.del_process_frame)
+        self.enter_process_for_delete = Entry(self.del_process_frame,font="arial 12 italic")
         self.enter_process_for_delete.grid(row=1, column=1)
 
     def delete_frame_update_memDiagram(self):
